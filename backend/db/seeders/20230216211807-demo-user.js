a9bd6d916da8c16acb8cf51f7dca9f7a6eb20a44
Tucker.Minson <Rtuckm@gmail.com>
2023-02-16 15:48:16 -0800

    add first and lastname columns to user

--- backend/db/seeders/20230216211807-demo-user.js
+++ backend/db/seeders/20230216211807-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: "Fakey",
+        lastName: "McUser",
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: "Inadda",
+        lastName: "Reeluser",
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: "Daphinatly",
+        lastName: "Naughtreel",
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
