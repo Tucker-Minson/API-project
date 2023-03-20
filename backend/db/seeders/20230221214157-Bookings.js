cba2f93bc527b2b28ab9ae66c19d38862d52e4e2
Tucker.Minson <Rtuckm@gmail.com>
2023-03-20 16:36:21 -0700

    updated booking seeder

--- backend/db/seeders/20230221214157-Bookings.js
+++ backend/db/seeders/20230221214157-Bookings.js
@@ -9,9 +9,15 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = 'Bookings';
     return queryInterface.bulkInsert(options, [
-      {spotId:2,userId:1,startDate:'2023-02-25',endDate:'2023-02-27',},
-      {spotId:1,userId:3,startDate:'2023-02-25',endDate:'2023-02-27',},
-      {spotId:3,userId:2,startDate:'2023-02-25',endDate:'2023-02-27',},
+      {spotId:1,userId:1,startDate:'2023-05-10',endDate:'2023-05-12',},
+      {spotId:2,userId:1,startDate:'2023-05-14',endDate:'2023-05-16',},
+      {spotId:3,userId:1,startDate:'2023-05-18',endDate:'2023-05-20',},
+      {spotId:1,userId:2,startDate:'2023-05-02',endDate:'2023-05-04',},
+      {spotId:2,userId:2,startDate:'2023-05-06',endDate:'2023-05-08',},
+      {spotId:3,userId:2,startDate:'2023-05-10',endDate:'2023-05-12',},
+      {spotId:1,userId:3,startDate:'2023-04-01',endDate:'2023-04-05',},
+      {spotId:2,userId:3,startDate:'2023-04-06',endDate:'2023-04-10',},
+      {spotId:3,userId:3,startDate:'2023-04-15',endDate:'2023-04-20',},
     ], {});
 
   },
