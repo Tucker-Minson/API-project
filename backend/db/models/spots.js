c96d3b53c1d58152d0cca49aa2f839601cc4cef2
Tucker.Minson <Rtuckm@gmail.com>
2023-03-31 12:54:48 -0700

    added allowNull:false back to ownerId column

--- backend/db/models/spots.js
+++ backend/db/models/spots.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   Spot.init({
     ownerId: {
       type:DataTypes.INTEGER,
+      allowNull: false
     },
     address: {
       type:DataTypes.STRING,
