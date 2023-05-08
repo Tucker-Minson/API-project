47d61f48934deb4f692d77879f4c59a259bc294a
Tucker.Minson <Rtuckm@gmail.com>
2023-05-08 12:59:22 -0700

    added thunk and action to create new spot

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,16 +1,17 @@
 
-export const RECEIVE_SPOTS = 'spots/RECEIVE_SPOTS';
-export const RECEIVE_SPOT = 'spots/RECEIVE_SPOT'
-
+const LOAD_SPOTS = 'spots/RECEIVE_SPOTS';
+const ADD_SPOT = 'spots/RECEIVE_SPOT'
+//////////////////// Actions ///////////////////////////////
 export const receiveSpots = (spots) => ({
-    type: RECEIVE_SPOTS,
+    type: LOAD_SPOTS,
     spots
 })
 export const receiveOneSpot = (id) => ({
-    type: RECEIVE_SPOT,
+    type: ADD_SPOT,
     id
 })
 
+//////////////////// Thunk ///////////////////////////////
 export const fetchSpots = () => async (dispatch) => {
     const res = await fetch('api/spots');
     const spots = await res.json();
@@ -29,22 +30,45 @@ export const getAllSpots = state => {
 }
 
 export const getOneSpot = (spot) => async dispatch => {
-const res = await fetch(`/api/spots/${spot.id}`);
-if (res.ok) {
-    const data = await res.json();
-    dispatch(receiveOneSpot(data))
-}
+    const res = await fetch(`/api/spots/${spot.id}`);
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(receiveOneSpot(data))
+    }
 }
 
+export const createSpotForm = (payload) => async dispatch => {
+    const res = await fetch('api/spots', {
+        headers: {'Content-Type': 'application/json'},
+        method: "POST",
+        body: JSON.stringify(payload)
+    })
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(receiveOneSpot(data))
+    }
+};
+///////////////////// Reducer //////////////////////////////
 const spotsReducer = (state = {}, action) => {
     switch (action.type) {
-        case RECEIVE_SPOTS: {
+        case LOAD_SPOTS: {
             const newState = { ...state };
             for (let spot of action.spots) {
                 newState[spot.id] = spot;
             }
             return newState
-        }
+        };
+        case ADD_SPOT: {
+            if (!state[action.spot.id]) {
+                const newState = {
+                    ...state,
+                    [action.spot.id]: action.spot
+                }
+                const spotList = newState.map(id => newState[id]);
+                spotList.push(action.spot);
+                return newState
+            }
+        };
         default: return state;
     }
 }
